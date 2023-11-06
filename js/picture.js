9b1a8089d377e3e9a3d63b3766dde42e9c6e179c
Ильшат Хуснутдинов <Leeeeenny01@gmail.com>
2023-11-06 20:51:19 +0300

    Открывается и закрывается (часть 2)

--- js/picture.js
+++ js/picture.js
@@ -1,8 +1,58 @@
+
+const COMMENT_COUNT_SHOW = 5;
+
 const bigPictureElement = document.querySelector('.big-picture');
 const bodyElement = document.querySelector('body');
 const closePictureButtonElement = bigPictureElement.querySelector('.big-picture__cancel');
 
+const commentsListElement = bigPictureElement.querySelector('.social__comments');
+const commentCountElement = bigPictureElement.querySelector('.social__comment-shown-count');
+const totalCommentCountElement = bigPictureElement.querySelector('.social__comment-total-count');
+const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
+
+let commentsCountShown = 0;
+let comments = [];
+const commentElement = document
+  .querySelector('#comment')
+  .content
+  .querySelector('.social__comment');
+
+const createComment = ({avatar, message, name}) => {
+  const newComment = commentElement.cloneNode(true);
+  newComment.querySelector('.social__picture').src = avatar;
+  newComment.querySelector('.social__picture').alt = name;
+  newComment.querySelector('.social__text').textContent = message;
+
+  return newComment;
+
+};
+
+const renderComments = () => {
+  commentsCountShown += COMMENT_COUNT_SHOW;
+ if(commentsCountShown >= comments.length) {
+  commentsLoaderElement.classList.add('hidden');
+  commentsCountShown = comments.length;
+ }
+ else {
+  commentsLoaderElement.classList.remove('hidden');
+ }
+ const fragment = document.createDocumentFragment();
+ for(let i = 0; i< commentsCountShown; i++) {
+  const comment = createComment(comments[i]);
+  fragment.append(comment);
+ }
+
+  commentsListElement.innerHTML = '';
+  commentsListElement.append(fragment);
+
+  commentCountElement.textContent = commentsCountShown;
+  totalCommentCountElement.textContent = comments.length;
+};
+
+const onCommentsLoaderClick = () => renderComments();
+
 const hidePicture = () => {
+  commentsCountShown = 0;
   bigPictureElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
@@ -31,9 +81,14 @@ const showPicture = (pictureData) => {
   bigPictureElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
+  comments = pictureData.comments;
+  if(comments.length > 0) {
+    renderComments();
+  }
+
   renderPicture(pictureData);
 };
 closePictureButtonElement.addEventListener('click',onClosePictureButtonClick);
-
+commentsLoaderElement.addEventListener('click',onCommentsLoaderClick);
 
 export{ showPicture };
